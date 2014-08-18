274cd39430c54babaec47225f5d2e77c8952d8d0
Tianon Gravi <admwiggin@gmail.com>
2014-08-18 15:43:48 -0600

    Add multiple versions, an update.sh script, and a generate-stackbrew-library.sh script

--- 2.2/docker-entrypoint.sh
+++ 2.2/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = 'mongod' ]; then
-	chown -R mongodb "/data/db"
+	chown -R mongodb /data/db
 	exec gosu mongodb "$@"
 fi
 
---
+++ 2.6/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'mongod' ]; then
+	chown -R mongodb /data/db
+	exec gosu mongodb "$@"
+fi
+
+exec "$@"
