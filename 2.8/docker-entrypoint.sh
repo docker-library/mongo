25b9c6dc5b96bec8429699215ef1f345f0fcfd34
Arnaud Porterie <arnaud.porterie@docker.com>
2015-01-10 23:39:54 -0800

    Support non NUMA enabled systems
    Signed-off-by: Arnaud Porterie <arnaud.porterie@docker.com>

--- 2.8/docker-entrypoint.sh
+++ 2.8/docker-entrypoint.sh
@@ -5,9 +5,15 @@ if [ "${1:0:1}" = '-' ]; then
 	set -- mongod "$@"
 fi
 
+CMD="numactl --interleave=all $@"
+if ! numactl --show &> /dev/null ; then
+    CMD="$@"
+    echo "Warning: no NUMA support available on this system."
+fi
+
 if [ "$1" = 'mongod' ]; then
 	chown -R mongodb /data/db
-	exec gosu mongodb numactl --interleave=all "$@"
+	exec gosu mongodb $CMD
 fi
 
 exec "$@"
