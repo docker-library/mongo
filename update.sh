102976234a6bd5d73bc568c7feb56f6338ebe2db
Tianon Gravi <admwiggin@gmail.com>
2015-03-04 09:47:22 -0700

    Switch 3.0 to use APT, as recommended by upstream

--- update.sh
+++ update.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 set -e
 
-declare -A gpgKeys
-gpgKeys=(
-	[2.2]='3AFEF01FE92B6927CC1EEC80F564179A36496327'
-	[2.4]='CEA1E18DDA77EF4E67884FF2A6982D0160456C5A'
-	[2.6]='DFFA3DCF326E302C4787673A01C4E7FAAAB2461C'
-	[3.0]='BDC0DB28022D7DEA1490DC3E7085801C857FD301'
-	
-	# unstable releases share their "major" tag's key
-	[2.1]='3AFEF01FE92B6927CC1EEC80F564179A36496327'
-	[2.3]='CEA1E18DDA77EF4E67884FF2A6982D0160456C5A'
-	[2.5]='DFFA3DCF326E302C4787673A01C4E7FAAAB2461C'
-	[2.7]='BDC0DB28022D7DEA1490DC3E7085801C857FD301'
-)
-# see https://www.mongodb.org/static/pgp/server-2.6.asc etc.
-
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( "$@" )
@@ -34,15 +19,11 @@ for version in "${versions[@]}"; do
 		packagesUrl='http://downloads-distro.mongodb.org/repo/debian-sysvinit/dists/dist/10gen/binary-amd64/Packages'
 	fi
 	fullVersion="$(curl -sSL "$packagesUrl.gz" | gunzip | grep -EA10 '^Package: mongodb-(org(-unstable)?|10gen)$' | grep "^Version: $version\." | cut -d' ' -f2 | sort -V | tail -1)"
-	gpgKey="${gpgKeys[$version]}"
-	if [ -z "$gpgKey" ]; then
-		echo >&2 "ERROR: missing GPG key fingerprint for $version; try:"
-		echo >&2 "  curl -sSL 'https://www.mongodb.org/static/pgp/server-$version.asc' | gpg --with-fingerprint -"
-		exit 1
-	fi
 	(
 		set -x
-		[ -z "$fullVersion" ] || sed -ri 's/^(ENV MONGO_VERSION) .*/\1 '"$fullVersion"'/' "$version/Dockerfile"
-		sed -ri 's/^(ENV MONGO_RELEASE_FINGERPRINT) .*/\1 '"$gpgKey"'/' "$version/Dockerfile"
+		sed -ri '
+			s/^(ENV MONGO_MAJOR) .*/\1 '"$version"'/;
+			s/^(ENV MONGO_VERSION) .*/\1 '"$fullVersion"'/;
+		' "$version/Dockerfile"
 	)
 done
