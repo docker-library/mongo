4ddbbe85acb75c027d6e455f9e9a5e0b7b85ad93
Anentropic <ego@anentropic.com>
2015-09-16 15:17:01 +0100

    postgresql-style pre-startup init scripts hook

--- 2.6/docker-entrypoint.sh
+++ 2.6/docker-entrypoint.sh
@@ -13,6 +13,20 @@ if [ "$1" = 'mongod' ]; then
 		set -- $numa "$@"
 	fi
 
+	# internal start of server in order to allow set-up using mongo client
+	gosu mongodb mongod --fork --dbpath=/data/db --syslog
+
+	for f in /docker-entrypoint-initdb.d/*; do
+		case "$f" in
+			*.sh)  echo "$0: running $f"; . "$f" ;;
+			*.js)  echo "$0: running $f"; mongo --nodb "$f" && echo ;;
+			*)     echo "$0: ignoring $f" ;;
+		esac
+	done
+
+	# stop the temporary server daemon
+	gosu mongodb mongod --shutdown --dbpath=/data/db
+
 	exec gosu mongodb "$@"
 fi
 
