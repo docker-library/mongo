4a81205a13fefc418355248f750551e4f7c62361
Joe Ferguson <joe@infosiftr.com>
2017-02-03 15:43:35 -0800

    mongos, mongodump, and friends are also valid for stepping down and numactl

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -6,12 +6,17 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 # allow the container to be started with `--user`
-if [ "$1" = 'mongod' -a "$(id -u)" = '0' ]; then
-	chown -R mongodb /data/configdb /data/db
+# all mongo* commands should be dropped to the correct user
+if [[ "$1" == mongo* ]] && [ "$(id -u)" = '0' ]; then
+	if [ "$1" = 'mongod' ]; then
+		chown -R mongodb /data/configdb /data/db
+	fi
 	exec gosu mongodb "$BASH_SOURCE" "$@"
 fi
 
-if [ "$1" = 'mongod' ]; then
+# you should use numactl to start your mongod instances, including the config servers, mongos instances, and any clients.
+# https://docs.mongodb.com/manual/administration/production-notes/#configuring-numa-on-linux
+if [[ "$1" == mongo* ]]; then
 	numa='numactl --interleave=all'
 	if $numa true &> /dev/null; then
 		set -- $numa "$@"
