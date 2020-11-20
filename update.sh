8afebb1ef44b8076f9f4472c689409835707399d
Tianon Gravi <admwiggin@gmail.com>
2020-11-20 15:34:15 -0800

    Doh!

--- update.sh
+++ update.sh
@@ -160,9 +160,9 @@ for version in "${versions[@]}"; do
 		Dockerfile-linux.template \
 		> "$version/Dockerfile"
 
-	# starting with MongoDB 4.3 (and backported to 4.0??), the postinst for server includes an unconditional "systemctl daemon-reload" (and we don't have anything for "systemctl" to talk to leading to dbus errors and failed package installs)
+	# starting with MongoDB 4.3 (and backported to 4.0 and 4.2??), the postinst for server includes an unconditional "systemctl daemon-reload" (and we don't have anything for "systemctl" to talk to leading to dbus errors and failed package installs)
 	case "$version" in
-		3.6 | 4.2)
+		3.6)
 			sed -i -e '/systemctl/d' "$version/Dockerfile"
 			;;
 	esac
