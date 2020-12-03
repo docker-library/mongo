502e24c7d8ca3529bc466be2c4a7b45c91357db9
Tianon Gravi <admwiggin@gmail.com>
2020-12-03 12:41:01 -0800

    Update gpg-keys.sh from stretch to buster

--- gpg-keys.sh
+++ gpg-keys.sh
@@ -5,7 +5,7 @@ versions=( $(grep -vE '^#|^$' gpg-keys.txt | cut -d: -f1) )
 
 for version in "${versions[@]}"; do
 	fingerprints="$(
-		docker run --rm -e v="$version" buildpack-deps:stretch-curl bash -Eeuo pipefail -xc '
+		docker run --rm -e v="$version" buildpack-deps:buster-curl bash -Eeuo pipefail -xc '
 			wget -O key.asc "https://www.mongodb.org/static/pgp/server-$v.asc" >&2
 			gpg --batch --import key.asc >&2
 			gpg --batch --fingerprint --with-colons | grep "^fpr:" | cut -d: -f10
